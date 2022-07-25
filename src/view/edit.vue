39ae9242cf38f4a2aac4dedd1ed8602718f9abbe
Administrator <admin@example.com>
2022-07-25 17:45:44 +0800

    1

---
+++ src/view/edit.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-dialog fullscreen center>
+        <template #header>
+            <div style="font-size: large">编辑</div>
+        </template>
+        <div>
+            此处放编辑{{code}}
+        </div>
+    </el-dialog>
+</template>
+
+<script setup lang="ts">
+    import {defineProps, onMounted, reactive, ref,} from "vue";
+
+    const props = defineProps({
+        type: String,
+        code: String,
+    })
+
+    const codeRef = ref();
+    const state = reactive({
+
+    });
+    onMounted(() => {
+        console.log("剪辑")
+    })
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/view/view.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-dialog fullscreen center>
+        <template #header>
+            <div style="font-size: large">视图查看</div>
+        </template>
+        <div>
+            此处放视图{{code}}
+        </div>
+    </el-dialog>
+</template>
+
+<script setup lang="ts">
+    import {defineProps, reactive, ref,onMounted} from "vue";
+
+    const props = defineProps({
+        type: String,
+        code: String,
+    })
+
+    const codeRef = ref();
+    const state = reactive({
+
+    });
+    onMounted(() => {
+        console.log("视图")
+    })
+
+</script>
+
+<style scoped>
+
+</style>
