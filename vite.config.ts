015c7369494d75c0e5f011499e622b386cfeeb63
PandaGoAdmin <18610165312@163.com>
2022-08-13 19:29:35 +0800

    完成拖拽ui更改

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,27 @@
-import { defineConfig } from 'vite'
+import { UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig: UserConfig = {
   plugins: [vue()],
+  base: './',
   resolve: {
     alias: {
       "@": "/src"
     }
   },
-})
+  build: {
+    //outDir: 'static',
+    minify: 'esbuild',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name].${new Date().getTime()}.js`,
+        chunkFileNames: `assets/[name].${new Date().getTime()}.js`,
+        assetFileNames: `assets/[name].${new Date().getTime()}.[ext]`,
+      },
+    },
+  },
+  css: { preprocessorOptions: { css: { charset: false } } },
+};
+
+export default viteConfig;
